232281344db4b8a79101acf5befcadd7bb0c3ac1
wuxiang <1325668250@qq.com>
2021-07-12 14:28:09 +0800

    增加网关模块

---
+++ cloud-gateway-gateway9527/src/main/java/com/aisino/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.aisino.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wuxiang
+ * @date 2021/7/12 2:19 下午
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
