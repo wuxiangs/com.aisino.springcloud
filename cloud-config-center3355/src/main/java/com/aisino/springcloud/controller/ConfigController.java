c4afcad4e480f1f58800308994e712138f3eafd7
wuxiang <1325668250@qq.com>
2021-07-13 14:26:51 +0800

    feat:增加功能

--- cloud-config-center3355/src/main/java/com/aisino/springcloud/controller/ConfigController.java
+++ cloud-config-center3355/src/main/java/com/aisino/springcloud/controller/ConfigController.java
@@ -19,6 +19,7 @@ public class ConfigController {
 
     @GetMapping(value="/config/getPort")
     public String getServerPort(){
+        System.out.println("张三");
         return serverPort;
     }
 }
